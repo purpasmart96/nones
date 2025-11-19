063db343892d4308eee4229461e6c45d8f95cdf2
Matt W <kanzoconfigz@hotmail.com>
2025-11-19 14:26:44 -0800

    Fix Battletoads regression. Will need to figure out the correct way to fix the odd even timing test from blargg.

--- src/system.c
+++ src/system.c
@@ -481,10 +481,10 @@ void SystemTick(void)
     APU_Tick(system_ptr->apu);
 
     PPU_Tick(system_ptr->ppu);
-    PpuUpdateRenderingState(system_ptr->ppu);
     SystemPollNmi(system_ptr);
     PPU_Tick(system_ptr->ppu);
     PPU_Tick(system_ptr->ppu);
+    PpuUpdateRenderingState(system_ptr->ppu);
 }
 
 void SystemAddCpuCycles(uint32_t cycles)
